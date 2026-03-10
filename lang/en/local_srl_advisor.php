c595fb8fb9de77f550af33dcce3661567cff673f
Andrew <aschwabe@gmail.com>
2026-03-10 10:50:07 +0700

    base app

---
+++ lang/en/local_srl_advisor.php
@@ -0,0 +1,7 @@
+ * Strings for component 'local_srl_advisor', language 'en'
+ *
+ * @package   local_srl_advisor
+ * @copyright 2026 SRL Advisor
+ */
+
+$string['pluginname'] = 'SRL Study Advisor';
---
+++ version.php
@@ -0,0 +1,13 @@
+ * SRL Study local plugin version information.
+ *
+ * @package    local_srl_advisor
+ * @copyright  2026 SRL Advisor
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2026031000; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2022112800; // Requires this Moodle version (Moodle 4.1).
+$plugin->component = 'local_srl_advisor'; // Full name of the plugin (used for diagnostics).
+$plugin->maturity  = MATURITY_ALPHA;
+$plugin->release   = 'v0.1.0-alpha';
